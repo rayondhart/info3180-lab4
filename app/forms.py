2e0db42fad041145ba77ba136c4e008746d292d8
Rayon Hart <114370781+rayondhart@users.noreply.github.com>
2025-03-20 14:35:41 -0500

    part 5

--- app/forms.py
+++ app/forms.py
@@ -6,3 +6,6 @@ from wtforms.validators import InputRequired
 class LoginForm(FlaskForm):
     username = StringField('Username', validators=[InputRequired()])
     password = PasswordField('Password', validators=[InputRequired()])
+
+class UploadForm(FlaskForm):
+    file = FileField('Image File', validators=[DataRequired(), FileAllowed(['jpg', 'png'], 'Images only!')])
